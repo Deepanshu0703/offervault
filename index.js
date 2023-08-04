708e789386f843538e6a9a7e117a7d7e58e0f255
Deepanshu0703 <21bcs072@iiitdmj.ac.in>
2023-08-04 17:46:05 +0530

    Added website

---
+++ index.js
@@ -0,0 +1,9 @@
+function showNetworksDropdown(){
+    document.getElementById("networks_dropdown_content").classList.toggle("show");
+}
+function showCategoiesDropdown(){
+    document.getElementById("category_dropdown_content").classList.toggle("show");
+}
+function showCountryDropDown(){
+    document.getElementById("country_dropdown_content").classList.toggle("show");
+}
